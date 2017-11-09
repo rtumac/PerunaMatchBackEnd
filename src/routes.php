3010118a63d398557f03acfde056f4796949f2fa
Ubuntu <ubuntu@ip-172-31-46-247.us-east-2.compute.internal>
2017-11-09 17:21:07 +0000

    initial commit

---
+++ src/routes.php
@@ -0,0 +1,55 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+// instantiate the App object
+//$app = new \Slim\App();
+
+$app->get('/todos', function ($request, $response, $args) {
+         $sth = $this->db->prepare("SELECT * FROM tasks ORDER BY task");
+        $sth->execute();
+        $todos = $sth->fetchAll();
+        return $this->response->withJson($todos);
+    });
+
+
+
+//routes:
+
+//welcome
+$app->get('/welcome', function ($request, $response, $args) {
+    return $response->withStatus(200)->write('Welcome to Peruna Projects!');
+});
+
+//student
+$app->get('/student/{name}', function($request, $response, $args) {
+	return $response->write("Student Profile for: " . $args['name']);
+});
+
+//professor
+$app->get('/professor/{name}', function($request, $response, $args) {
+        return $response->write("Professor Profile for: " . $args['name']);
+});
+
+//listings
+$app->get('/listings', function ($request, $response, $args) {
+    return $response->withStatus(200)->write("My Listings");
+});
+
+//settings
+$app->get('/{name}/settings', function($request, $response, $args) {
+        return $response->write("Settings for user: " . $args['name']);
+});
+
+// Run application
+//$app->run();
+
+// default
+$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
