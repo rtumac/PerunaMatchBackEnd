833087478cfef590dcd6afac747409e32749fc7b
Remus Tumac <rtumac@smu.edu>
2017-11-23 17:36:00 +0000

    DB Server Transitioned from RDS to EC2

--- src/routes.php
+++ src/routes.php
@@ -7,13 +7,18 @@ use Slim\Http\Response;
 //$app = new \Slim\App();
 
 $app->get('/todos', function ($request, $response, $args) {
-         $sth = $this->db->prepare("SELECT * FROM tasks ORDER BY task");
+         $sth = $this->db->prepare("SELECT * FROM Users");
         $sth->execute();
         $todos = $sth->fetchAll();
         return $this->response->withJson($todos);
     });
 
+/*
+$app->post('/login',
+	function($request, $response, $args) {
 
+	});
+*/
 
 //routes:
 
@@ -46,10 +51,10 @@ $app->get('/{name}/settings', function($request, $response, $args) {
 //$app->run();
 
 // default
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+/*$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
-});
+});*/
